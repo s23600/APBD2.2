c74ba763cc6c33e67f5828dee9583042ba64fb96
= <=>
2023-03-15 15:25:21 +0100

    first commit

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+using System
+
+namespace Uni
+{
+	public Student()
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+}
+}
---
+++ Ex.2/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
+namespace Uni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+
+            FileInfo fileInfo = new(path);
+            using StreamReader streamReader = new(fileInfo.OpenRead());
+
+            List<string> lines = new();
+            string fileLine;
+            while((fileLine =streamReader.ReadLine()) !=null)
+            {
+                lines.Add(fileLine);
+
+                string[] studentValues = fileLine.Split(',');
+                Student student = new()
+                {
+                   FirstName = studentValues[0],
+                   LastName = studentValues[1]
+                }
+            }
+            foreach(string line in lines) 
+            { 
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
